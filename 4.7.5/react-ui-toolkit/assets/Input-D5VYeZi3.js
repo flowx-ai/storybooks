aa8013bf049bbf07a56f0863e45ec5233363a981
Mircea Bardac <mircea@flowx.ai>
2025-06-20 22:40:24 +0300

    docs: storybooks for FlowX 4.7.5 / v5.95.1

---
+++ 4.7.5/react-ui-toolkit/assets/Input-D5VYeZi3.js
@@ -0,0 +1,25 @@
+import{j as e}from"./jsx-runtime-CBDCb4rg.js";import{useMDXComponents as r}from"./index-BSj771as.js";import{ae as i,af as p,ag as s}from"./index-Bbn6NGYK.js";import{I as a,a as l}from"./Input.stories-CCzXvnG1.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-l3PPIgH2.js";import"../sb-preview/runtime.js";import"./index-DjIwiQkP.js";import"./index-CLC3E5cw.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./useComputedClearIcon-CXkq2o3F.js";import"./Icon-DQNTWSJm.js";import"./Content-DlMT5peK.js";import"./index-qMorCPSe.js";import"./Portal-vLh0jiGk.js";function o(n){const t={code:"code",h1:"h1",h2:"h2",pre:"pre",...r(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:a}),`
+`,e.jsx(t.h1,{id:"input",children:"Input"}),`
+`,e.jsx(t.h2,{id:"imports",children:"Imports"}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-ts",children:`import { FlxInput } from '@flowx/react-ui-toolkit'
+`})}),`
+`,e.jsx(t.h2,{id:"inputs",children:"Inputs"}),`
+`,e.jsx(p,{children:`
+| Name        | Description                 |
+| ----------- | --------------------------- |
+| type        | "text" / "number"           |
+| label       | Label text                  |
+| disabled    | Disable input               |
+| helper      | Helper text                 |
+| infoTooltip | Move helper text to tooltip |
+| hasClear    | Show clear button           |
+| prefix      | Prefix text                 |
+| suffix      | Suffix text                 |
+| prefixIcon  | Prefix icon                 |
+| suffixIcon  | Suffix icon                 |
+`}),`
+`,e.jsx(t.h2,{id:"usage",children:"Usage"}),`
+`,e.jsx(s,{of:l}),`
+`,e.jsx(t.h2,{id:"example",children:"Example"}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-html",children:`<FlxInput type="text" label="Label" helper="Helper text" />
+`})})]})}function F(n={}){const{wrapper:t}={...r(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{F as default};

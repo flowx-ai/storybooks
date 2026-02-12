4302f5cab61673deb8e6c6fe1376006ce2693fb6
Mircea Bardac <mircea@flowx.ai>
2026-02-12 18:44:07 +0200

    docs: storybooks for FlowX 5.1.4 / v9.163.6

---
+++ 5.1.4/react-ui-toolkit/assets/FieldItemContext-FDB5z_De.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-Bv9Y92EF.js";const e=t.createContext({disabled:!1});function r(){return t.useContext(e)}export{r as u};
---
+++ 5.1.4/react-ui-toolkit/assets/Message-3x7SBDaQ.js
@@ -0,0 +1,21 @@
+import{j as e}from"./jsx-runtime-DztICxWZ.js";import{useMDXComponents as r}from"./index-BghErDIA.js";import{M as t,a as i,S as a}from"./index-7OAH4CM1.js";import{M as c,a as m}from"./Message.stories-_jp0TCTA.js";import"./index-Bv9Y92EF.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-DM0CN2bs.js";import"./index-DZfHsFkt.js";import"./index-CJSLNY_5.js";import"./index-ChPI8MDz.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./Icon-D7WurJMX.js";function o(n){const s={code:"code",h1:"h1",h2:"h2",pre:"pre",...r(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t,{of:c}),`
+`,e.jsx(s.h1,{id:"message",children:"Message"}),`
+`,e.jsx(s.h2,{id:"import",children:"Import"}),`
+`,e.jsx(s.pre,{children:e.jsx(s.code,{className:"language-ts",children:`import { FlxMessage } from '@flowx/ui-toolkit/react'
+`})}),`
+`,e.jsx(s.h2,{id:"inputs",children:"Inputs"}),`
+`,e.jsx(i,{children:`
+| Name             | Description                              | Default | Comments       |
+| ---------------- | ---------------------------------------- | ------- | -------------- |
+| type             | "success" / "warning" / "error" / "info" | success |                |
+| display          | "text" / "border" / "fill"               | border  |                |
+| layout           | "row"/ "column"                          | row     |                |
+| iconComponent    | ReactElement                             | -       | icon component |
+`}),`
+`,e.jsx(s.h2,{id:"usage",children:"Usage"}),`
+`,e.jsx(a,{of:m}),`
+`,e.jsx(s.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(s.pre,{children:e.jsx(s.code,{className:"language-tsx",children:`<FlxMessage type="success">
+  This is a success message
+</FlxMessage>
+`})})]})}function D(n={}){const{wrapper:s}={...r(),...n.components};return s?e.jsx(s,{...n,children:e.jsx(o,{...n})}):o(n)}export{D as default};
